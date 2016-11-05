20883ea4ff34f1bc5fa6e1f5ca7315ada834b181
Nathan Teetor <nathanteetor@gmail.com>
2016-11-05 16:53:15 -0400

    fixed tests

--- tests/testthat/test-load.R
+++ tests/testthat/test-load.R
@@ -2,18 +2,18 @@ context('parsing file')
 
 test_that('error when missing definition', {
   e <- new.env(parent = baseenv())
-  expect_error(presents('../testfiles/missing-definitions.R', envir = e), 'timer')
+  expect_error(source_decorated('../testfiles/missing-definitions.R', envir = e), 'timer')
 })
 
 test_that('non-decorated functions skipped', {
   e <- new.env(parent = baseenv())
-  presents('../testfiles/sample-functions.R', envir = e)
+  source_decorated('../testfiles/sample-functions.R', envir = e)
   expect_true(!exists('g', envir = e, inherits = FALSE))
 })
 
 test_that('decorated functions loaded', {
   e <- new.env(parent = baseenv())
-  presents('../testfiles/sample-functions.R', envir = e)
+  source_decorated('../testfiles/sample-functions.R', envir = e)
   expect_true(exists('f', envir = e, inherits = FALSE))
   expect_true(exists('h', envir = e, inherits = FALSE))
 })
