d0b6aac552a16e743880808a144cd26d52865f2f
Nathan Teetor <nathanteetor@gmail.com>
2016-11-16 13:19:34 -0500

    added test coverage for `tinsel_example` and `tinsel_examples`

---
+++ tests/testthat/test-example.R
@@ -0,0 +1,14 @@
+context('testing example helpers')
+
+test_that('file path exists', {
+  expect_true(file.exists(tinsel_example('tags.R')))
+})
+
+test_that('error for incorrect name', {
+  expect_error(tinsel_example('WHOOPS.R'))
+})
+
+test_that('`tinsel_examples` lists all files', {
+  expect_equal(tinsel_examples(), c('attributes.R', 'tags.R', 'timer.R'))
+})
+
